1f61c1f4e0e08daaf31170a6ffdc1e2541f69e6d
Stefferson Thallys <steffersonthallys@gmail.com>
2023-06-19 01:04:55 +0100

    fix update environment dto

--- src/environments/dto/update-environment.dto.ts
+++ src/environments/dto/update-environment.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEnvironmentDto } from './create-environment.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { Status } from '../entities/status.enum';
 
-export class UpdateEnvironmentDto extends PartialType(CreateEnvironmentDto) {}
+export class UpdateEnvironmentDto extends PartialType(CreateEnvironmentDto) {
+  @ApiProperty({ type: 'enum', enum: Status, example: Status.AVAILABLE, required: true })
+  @IsEnum(Status)
+  @IsOptional()
+  status: Status;
+}
